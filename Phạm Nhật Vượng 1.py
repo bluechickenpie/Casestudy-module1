998020c0dee6ee1e7f9863e6e898ea8e5a2c8b5d
bluechickenpie <165547083+bluechickenpie@users.noreply.github.com>
2024-05-19 09:24:44 +0700

    Completed Code for Ai Là Triệu Phú
    check it out!

---
+++ Phạm Nhật Vượng 1.py
@@ -0,0 +1,152 @@
+print ("chào mừng các bạn đã đến với chò trơi AI LÀ PHẠM NHẬT VƯỢNG!!!")
+print ("luật trò chơi như sau:")
+print ("+mới vào bạn sẽ có 0 điểm.")
+print ("+mỗi câu trl đúng sẽ cho bạn 1 điểm, tương đương với 100 đô la")
+print ("+câu trả lời sai sẽ bị trừ 1 điểm, trả lời ko đúng yêu cầu sẽ ko có điểm, điểm tổng kết ra âm thì phải trả tiền cho chương trình >:)")
+print ("Các bạn hãy tự tính số tiền thưởng/phạt mình có sau khi tổng kết nhé!")
+print ("!!!chúc bạn may mắn!!!")
+
+score = 0
+print ("Câu 1: Đâu là năm đất nước ta thành công giải phóng miền Nam? A.1975, B.1954, C.1969, D.1945")
+ans=input("xin mời bạn nhập vào câu trả lời:")
+print (ans)
+if (ans == "A"):
+    print ("Câu trả lời chính xác!")
+    score = score + 1
+elif (ans == "a"):
+    print ("viết hoa đi rùi tui tính điểm cho nha :3")
+    score = score
+else:
+    print ("câu trả lời sai")
+    score = score - 1
+    
+    
+print ("Câu 2: Ai là người đã lấy thân mình lấp lỗ châu mai? A.Võ Nguyên Giáp, B.Phan Đình Giót, C.Phạm Nhật Vượng, D.Kim Đồng:")
+ans=input("xin mời bạn nhập câu trả lời:")
+print (ans)
+if (ans == "B"):
+    print ("Đù mé anh này Nảo to @_@")
+    score = score + 1
+elif (ans == "b"):
+    print ("viết hoa đi rùi tui tính điểm cho nha :3")
+    score = score
+else:
+    print ("sai rùi nhe")
+    score = score - 1
+    
+    
+print ("Câu 3:chiến dịch nào thể hiện cho sự thắng lợi của ta với Thực Dân Pháp? A.Điện Biên Phủ, B.Thượng Lào, C.Hồ Chí Minh, D.265(Tây Nguyên) :")
+ans=input("xin mời bạn nhập vào câu trả lời:")
+print (ans)
+if (ans == "A"):
+    print ("đúng gòi nha bạn ^^")
+    score = score + 1
+elif (ans == "a"):
+    print ("viết hoa đi rùi tui tính điểm cho nha :3")
+    score = score
+else:
+    print ("sai r nha bẹn ;)")
+    score = score - 1
+    
+print ("Câu 4: Người thanh niên Nguyễn Tất Thành ra đi tìm đường cứu nước năm bao nhiêu? A.1870, B.1969, C.1911, D.1905")
+ans=input("xin mời bạn nhập vào câu trả lời:")
+print (ans)
+if (ans == "C"):
+    print ("Câu trả lời chính xác!")
+    score = score + 1
+elif (ans == "c"):
+    print ("viết hoa đi rùi tui tính điểm cho nha :3")
+    score = score
+else:
+    print ("câu trả lời sai")
+    score = score - 1
+
+print ("Câu 5:Ai là tác giả của tập thơ Hương Cây-Bếp Lửa? A.Xuân Diệu, B.Bằng Việt, C.Ngô Tất Tố, D.Huy Cận")
+ans=input("xin mời bạn nhập vào câu trả lời:")
+print (ans)
+if (ans == "B"):
+    print ("Câu trả lời chính xác!")
+    score = score + 1
+elif (ans == "b"):
+    print ("viết hoa đi rùi tui tính điểm cho nha :3")
+    score = score
+else:
+    print ("câu trả lời sai")
+    score = score - 1
+
+print ("Câu 6:Hoàn cảnh ra đời của bài thơ Ngắm Trăng của Hồ Chí Minh là? A.Khi Bác đang ở chiến khu Việt Bắc, B.khi Bác đang ở trong hang Pác Bó, C.khi Bác đang ngồi trong nhà tù Tưởng Giới Thạch, D.Khi Bác còn đang ở Pháp")
+ans=input("xin mời bạn nhập vào câu trả lời:")
+print (ans)
+if (ans == "C"):
+    print ("Câu trả lời chính xác!")
+    score = score + 1
+elif (ans == "c"):
+    print ("viết hoa đi rùi tui tính điểm cho nha :3")
+    score = score
+else:
+    print ("câu trả lời sai")
+    score = score - 1
+
+print ("Câu 7: Loài động vật tượng trưng cho hình ảnh tự do trong bài thơ Nhớ Rừng của Thế Lữ là? A.Chim bồ câu, B.Gấu, C.Cáo, D.Hổ")
+ans=input("xin mời bạn nhập vào câu trả lời:")
+print (ans)
+if (ans == "D"):
+    print ("Câu trả lời chính xác!")
+    score = score + 1
+elif (ans == "d"):
+    print ("viết hoa đi rùi tui tính điểm cho nha :3")
+    score = score
+else:
+    print ("câu trả lời sai")
+    score = score - 1
+
+print ("Câu 8:Đoàn Thuyền đánh cá được Huy Cận viết khi đi trải nghiệm thực tế ở đâu? A Quảng Ninh, B.Nha Trang, C.Vũng Tàu, D.Cát Bà")
+ans=input("xin mời bạn nhập vào câu trả lời:")
+print (ans)
+if (ans == "A"):
+    print ("Câu trả lời chính xác!")
+    score = score + 1
+elif (ans == "a"):
+    print ("viết hoa đi rùi tui tính điểm cho nha :3")
+    score = score
+else:
+    print ("câu trả lời sai")
+    score = score - 1
+
+print ("Câu 9: Truyện Người Con Gái Nam Xương nằm trong tập thơ nào? A.Truyện Kiều , B.Lửa Thiêng, C.Truyền kì mạn lục, D.Vợ chàng Trương")
+ans=input("xin mời bạn nhập vào câu trả lời:")
+print (ans)
+if (ans == "C"):
+    print ("Câu trả lời chính xác!")
+    score = score + 1
+elif (ans == "c"):
+    print ("viết hoa đi rùi tui tính điểm cho nha :3")
+    score = score
+else:
+    print ("câu trả lời sai")
+    score = score - 1
+
+print ("Câu 10: nhân vật chính trong Những Ngôi Sao xa xôi của Lê Minh Khuê là? A.Phương Định, B.Thao, C.Nho, D.Lê Minh khuê")
+ans=input("xin mời bạn nhập vào câu trả lời:")
+print (ans)
+if (ans == "A"):
+    print ("Câu trả lời chính xác!")
+    score = score + 1
+elif (ans == "a"):
+    print ("viết hoa đi rùi tui tính điểm cho nha :3")
+    score = score
+else:
+    print ("câu trả lời sai")
+    score = score - 1
+print ("số điểm tổng kết của bạn là:")
+print (score)
+if (score >= 5 and score <= 9):
+    print ("khá đấy bạn hiền :D")
+elif (score == 10):
+    print ("Quá tuyệt vời bạn ơi!!!")
+elif (score == 4):
+    print ("cũng được đấy")
+elif (score <= 3):
+    print ("ko có chi phải nản hết! hãy cố gắng lần sau nhé!!! ^^")
+print ("!!cảm ơn các bạn đã tham gia trò chơi!!!")
+print ("Cre:Bluechickenpie")
